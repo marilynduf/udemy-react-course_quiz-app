abeb7ed147a1d1275fea17d56927fb3a7c9c9d8d
M <marilynduf@gmail.com>
2025-10-18 16:14:31 -0400

    Answers choices shuffled + hand of quiz handle without 'crashing' app

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,13 +4,17 @@ import QUESTIONS from "../questions";
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     const activeQuestionIndex = userAnswers.length;
-    const currentQuestion = QUESTIONS[activeQuestionIndex].text;
+    const quizDone = activeQuestionIndex === QUESTIONS.length;
+
+    if (quizDone) {
+        return <section>Reasult</section>;
+    }
 
     function handleOnclick(answer) {
         setUserAnswers((prevState) => [...prevState, answer]);
     }
 
-    const display = QUESTIONS[activeQuestionIndex].answers.map(
+    const answers = QUESTIONS[activeQuestionIndex].answers.map(
         (answer, index) => (
             <li key={index} className="answer">
                 <button onClick={() => handleOnclick(answer)} key={answer}>
@@ -20,12 +24,17 @@ export default function Quiz() {
         )
     );
 
+    const currentQuestion = QUESTIONS[activeQuestionIndex].text;
+    const suffledAnswers = [...answers].sort(() => Math.random() - 0.5);
+
     return (
-        <div id="quiz">
-            <div id="question">
-                <h2>{currentQuestion}</h2>
-                <ul id="answers">{display}</ul>
-            </div>
-        </div>
+        <>
+            <section id="quiz">
+                <div id="question">
+                    <h2>{currentQuestion}</h2>
+                    <ul id="answers">{suffledAnswers}</ul>
+                </div>
+            </section>
+        </>
     );
 }
